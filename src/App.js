57648c96f574064d3b749d1bd6dbbe517c90ab8d
rayhans358 <rayhans358@gmail.com>
2023-05-09 21:25:39 +0700

    Success deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br />Success deploy
+          <br />
+          Success deploy
         </p>
         <a
           className="App-link"
